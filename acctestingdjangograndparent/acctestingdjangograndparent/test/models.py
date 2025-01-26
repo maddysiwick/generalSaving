1b8839dbce024843231cb417818ea804786dc0d9
maddy <maddy.siwick@icloud.com>
2025-01-26 15:33:29 -0500

    uhh got smth ig

--- acctestingdjangograndparent/acctestingdjangograndparent/test/models.py
+++ acctestingdjangograndparent/acctestingdjangograndparent/test/models.py
@@ -10,5 +10,6 @@ class Language(models.Model):
 class Doctor(models.Model):
     name=models.CharField(max_length=100)
     languages=models.ManyToManyField(Language)
+    access=models.BooleanField(default=True)
     def __str__(self):
         return self.name
